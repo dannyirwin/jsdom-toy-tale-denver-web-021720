3b4743baf361fccf731baa0574dcbd72e8e91520
dannyirwin <dirwin123@gmail.com>
2021-04-28 14:26:25 -0600

    re-formatted for readability

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+
+fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(json => json.forEach(toy => {
+    createCard(toy);
+  }));
+
+
+
+function createCard(toy) {
+  console.log(toy);
+  const card = document.createElement("div");
+  const name = document.createElement("h2");
+  const image = document.createElement("img");
+  const likes = document.createElement("p");
+  const likeButton = document.createElement("button");
+
+  name.textContent = toy.name;
+  image.src = toy.image;
+  image.classList.add("toy-avatar");
+  likes.textContent = `${toy.likes} Likes`;
+  likeButton.textContent = "Like";
+  likeButton.classList.add("like-btn");
+
+  card.classList.add("card");
+  card.appendChild(name);
+  card.appendChild(image);
+  card.appendChild(likes);
+  card.appendChild(likeButton);
+
+  document.getElementById("toy-collection").appendChild(card);
+}
+
+function addNewToy(toy) {
+
+}
+
+
+
+//---------------
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
